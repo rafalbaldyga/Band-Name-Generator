a104efc571e6fdd1631f65263cd32d517bebe9ee
rafalbaldyga <rafalbaldyga20@gmail.com>
2022-05-01 17:01:49 +0200

    Add files via upload

---
+++ Band name generator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Welcome to the Band Name Generator")
+a = input("What's name of the city you grew up in? \n")
+b = input("What's your pet's name? \n")
+print("Your band name could be " + a + " " + b)
